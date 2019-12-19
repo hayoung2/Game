935dea32ce0c425769f1ced888d1e4a1577f6334
hayoung298 <dudgk298@gmail.com>
2019-12-20 00:41:58 +0900

    프로젝트 생성

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Animator animator;
+
+    void Start()
+    {
+        animator = this.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left * 1 * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right * 1 * Time.deltaTime);
+        }
+    }
+
+}
