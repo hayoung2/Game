022886108c81ebe207d0e6869e704f16973e74d7
hayoung298 <dudgk298@gmail.com>
2019-12-23 21:24:49 +0900

    모든버튼관리하는 클래스 추가
    setting, 시작 종료 등

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonManager : MonoBehaviour
+{
+    bool isStart = false;
+
+    public bool GetIsStart()
+    {
+        return isStart;
+    }
+
+    public void SetIsStart(bool end)
+    {
+        this.isStart = end;
+    }
+
+    public void GameStart()
+    {
+        isStart = true;
+    }
+}
