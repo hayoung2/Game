bdea544a5357999dae5bb24e7a555c21f6d2e868
hayoung298 <dudgk298@gmail.com>
2019-12-23 21:25:24 +0900

    플레이어 코드 수정
    시작 버튼 후 움직임 가능 구현

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,29 +6,34 @@ public class Player : MonoBehaviour
 {
     public GameObject anim;
     private Animator animator;
+    private ButtonManager startBtn;
 
     void Start()
     {
         animator = anim.GetComponent<Animator>();
+        startBtn = GetComponent<ButtonManager>();
     }
 
     void Update()
     {
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (startBtn.GetIsStart())
         {
-            WalkAnimation();
-            transform.Translate(Vector3.left * 1 * Time.deltaTime);
-        }
-        else if (Input.GetKey(KeyCode.RightArrow))
-        {
-            WalkAnimation();
-            transform.Translate(Vector3.right * 1 * Time.deltaTime);
-        }
-        else if (Input.GetKey(KeyCode.UpArrow))
-        {
-            JumpAnimation();
-            transform.Translate(Vector3.up * 4 * Time.deltaTime);
-            
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                WalkAnimation();
+                transform.Translate(Vector3.left * 1 * Time.deltaTime);
+            }
+            else if (Input.GetKey(KeyCode.RightArrow))
+            {
+                WalkAnimation();
+                transform.Translate(Vector3.right * 1 * Time.deltaTime);
+            }
+            else if (Input.GetKey(KeyCode.UpArrow))
+            {
+                JumpAnimation();
+                transform.Translate(Vector3.up * 4 * Time.deltaTime);
+
+            }
         }
     }
     void WalkAnimation()
